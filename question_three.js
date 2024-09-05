7f161a2437d30d11fe260e8312b83e1b8d8a0cbb
Gabriel Minatto <gabrielminatto3@gmail.com>
2024-09-05 04:51:04 -0300

    [TARGET-3] Correção data.json

--- question_three.js
+++ question_three.js
@@ -11,7 +11,7 @@ function loadInovoiceData() {
 function calculateInvoicing() {
     const monthlyInvoice = loadInovoiceData();
 
-    const billingDays = monthlyInvoice.filter(d => d.faturamento > 0);
+    const billingDays = monthlyInvoice.filter(d => d.valor > 0);
 
     let lowerRevenueValue = Infinity;
     let lowerRevenueDay;
@@ -20,25 +20,25 @@ function calculateInvoicing() {
     let sum = 0;
 
     for (let day of billingDays) {
-        sum += day.faturamento;
+        sum += day.valor;
 
-        if (higherRevenueValue < day.faturamento) {
-            higherRevenueValue = day.faturamento;
+        if (higherRevenueValue < day.valor) {
+            higherRevenueValue = day.valor;
             higherRevenueDay = day.dia;
         }
 
-        if (lowerRevenueValue > day.faturamento) {
-            lowerRevenueValue = day.faturamento;
+        if (lowerRevenueValue > day.valor) {
+            lowerRevenueValue = day.valor;
             lowerRevenueDay = day.dia;
         }
     }
 
     let averageRevenue = sum/billingDays.length;
 
-    const daysAboveAverage = billingDays.filter(day => day.faturamento > averageRevenue).length;
+    const daysAboveAverage = billingDays.filter(day => day.valor > averageRevenue).length;
 
-    console.log(`Menor faturamento foi dia: ${higherRevenueDay} - R$ ${higherRevenueValue} `);
-    console.log(`Maior faturamento foi dia: ${lowerRevenueDay} - R$ ${lowerRevenueValue} `);
+    console.log(`Menor faturamento foi dia: ${higherRevenueDay} - R$ ${higherRevenueValue.toFixed(2)} `);
+    console.log(`Maior faturamento foi dia: ${lowerRevenueDay} - R$ ${lowerRevenueValue.toFixed(2)} `);
     console.log(`Número de dias com faturamento acima da média: ${daysAboveAverage}`);
 }
 
